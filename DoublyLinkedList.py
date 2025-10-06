931dec5a79fc2e80505c68048f7da835be96d1c6
maha <mahasobuh2003@gmail.com>
2025-10-06 11:08:04 +0300

    delete

--- DoublyLinkedList.py
+++ DoublyLinkedList.py
@@ -14,11 +14,6 @@ class LinkedList :
         if initial_values : 
             for value in initial_values : 
                 self.insert_end(value)
-   
-   
-    def _add_node(self, node):
-        self.debug_data.append(node)
-        self.length += 1
 
     def print (self): 
         cur=self.head 
@@ -41,6 +36,16 @@ class LinkedList :
             first.next=second 
         if second: 
             second.prev=first 
+    
+    def _delete_and_link(self,node): 
+        if not node : 
+            return 
+        isTail= node==self.tail
+        prev=node.prev 
+        self._link(prev,node.next)
+        self.length-=1 
+        if isTail : 
+            self.tail=prev
 
     def insert_end(self,value) : 
         node = Node(value)
@@ -49,16 +54,65 @@ class LinkedList :
         else : 
             self._link(self.tail,node)
             self.tail=node 
+        self.length +=1 
     
     def insert_front(self,value): 
         node=Node(value)
+        self.length+=1 
         self._link(node,self.head) 
         self.head=node 
         if self.length == 1 : 
             self.head=node 
 
+    def insert_sorted(self,value) : 
+        if self.length==0 or value<=self.head.data: 
+            self.insert_front(value)
+        elif self.tail.data <=value : 
+            self.insert_end(value)
+        else : 
+            prev , cur = self.head , self.head.next 
+            node=Node(value)
+            while cur : 
+                if value <= cur.data : 
+                    self._link(node,cur)
+                    self._link(prev,node)
+                    self.length +=1 
+                    break 
+                prev=prev.next 
+                cur=cur.next 
+    
+    def delete_front(self): 
+        if not self.head : 
+            return 
+        next=self.head.next
+        self._delete_and_link(self.head)
+        self.head=next
+
+        if self.head : 
+            self.head.prev=None 
+        if self.length<=1 : 
+            self.tail=self.head
+
+    def delete_last(self): 
+        if self.length <=1: 
+            self.delete_front()
+
+        prevu=self.tail.prev
+        self.tail=prevu
+        self.tail.next=None
+        self.length-=1 
+
 
 #################################################
 lst = LinkedList([1, 2, 3, 4, 5])
+print(lst.length )
 lst.insert_front(20)
 lst.print()
+print(lst.length)
+lst2 = LinkedList([1, 5,10,20])
+print (lst2.length)
+lst2.delete_last()
+lst2.print()
+print (lst2.length)
+
+
